254bc43f80fe445dc0b779a0594d1f331245ee5d
Anita <anita@anitapatel.com>
2012-02-13 14:25:27 -0500

    bugs with zipped downloads

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -273,7 +273,7 @@ class JobsController < ApplicationController
     
     Zip::ZipFile.open(@zip_file_path, Zip::ZipFile::CREATE) do |zipfile|
       @applicants.each do |applicant|
-        if File.file?("#{applicant.last_name}_#{applicant.first_name}")
+        if File.directory?("#{applicant.last_name}_#{applicant.first_name}")
           File.delete("#{applicant.last_name}_#{applicant.first_name}")
         end
         zipfile.mkdir("#{applicant.last_name}_#{applicant.first_name}")  
