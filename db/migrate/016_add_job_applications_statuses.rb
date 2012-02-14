82f523b96487f81e221d7b6c8fd5cd51b7842c65
Anita <anita@anitapatel.com>
2012-02-14 11:47:29 -0500

    added new status dropdowns

---
+++ db/migrate/016_add_job_applications_statuses.rb
@@ -0,0 +1,13 @@
+class AddJobApplicationsStatuses < ActiveRecord::Migration
+  def self.up
+    add_column :job_applications, :offer_status, :string
+    add_column :job_applications, :review_status, :string
+    remove_column :job_applications, :acceptance_status
+  end
+
+  def self.down
+    remove_column :jobs, :offer_status
+    remove_column :jobs, :review_status
+    add_column :job_applications, :acceptance_status, :string
+  end
+end
